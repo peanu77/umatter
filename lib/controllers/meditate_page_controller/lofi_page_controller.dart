64eda233707400bf0fe2ebaedab0478eb058399b
pinat <peanut.renoangelo.banderlipe@gmail.com>
2022-03-03 19:24:45 +0800

    added controller for audio

---
+++ lib/controllers/meditate_page_controller/lofi_page_controller.dart
@@ -0,0 +1,11 @@
+import 'package:umatter/models/meditate_page_models/lofi_page_info.dart';
+
+class LofiPageController {
+  List<LofiPageInfo> lofiPageInfo = [
+    LofiPageInfo(
+        title: "Lofi title 1",
+        coverImg: "",
+        music: "audio/lofi.mp3",
+        authorName: "Lofi")
+  ];
+}
