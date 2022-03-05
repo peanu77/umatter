69a6889c2564174fdef9d070fc10ed76c5931fd1
pinat <peanut.renoangelo.banderlipe@gmail.com>
2022-03-05 09:01:55 +0800

    added quotes page

---
+++ lib/views/quotes_page/quotes_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QuotesPage extends StatelessWidget {
+  const QuotesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('asd'),
+      ),
+    );
+  }
+}
