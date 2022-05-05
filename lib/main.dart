03db894e4c7f8595c242d4898855eb33f0accf1b
pinat <peanut.renoangelo.banderlipe@gmail.com>
2022-05-05 23:39:05 +0800

    removed GetX implementation for onboarding screen, remove redundant code, removed comments, removed unecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:umatter/auth/auth_pages/login_page_controller.dart';
-import 'package:umatter/controllers/dashboard_controller/dashboard_binding.dart';
 import 'package:umatter/controllers/shared_pref_controller/shared_pref_controller.dart';
 import 'package:umatter/views/onboarding_screen/onboarding_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 int? isViewed;
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Prevent the screen to rotate
@@ -27,18 +22,6 @@ void main() async {
 
   await Firebase.initializeApp();
 
-  var initAndroidSettings =
-      const AndroidInitializationSettings("launcher_logo");
-
-  var initializationSettings =
-      InitializationSettings(android: initAndroidSettings);
-
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-      onSelectNotification: (payload) async {
-    if (payload != null) {
-      debugPrint("Notification payload : $payload");
-    }
-  });
   runApp(const MyApp());
 }
 
@@ -47,22 +30,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // textTheme: GoogleFonts.montserratTextTheme(),
         textTheme: GoogleFonts.montserratTextTheme(),
-        // primaryColor: kPrimary,
       ),
-      initialRoute: '/',
-      getPages: [
-        GetPage(
-          name: '/',
-          page: () =>
-              isViewed != 0 ? const OnboardingPage() : const LogInPage(),
-          binding: DashBoardBinding(),
-        ),
-      ],
+      home: isViewed != 0 ? const OnboardingPage() : const LogInPage(),
     );
   }
 }
